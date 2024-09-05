4636155e6e278c852dc4b0c9cd208011420e6046
Dumzi <105924691+Dumezweni-M@users.noreply.github.com>
2024-09-05 13:55:13 +0200

    Signup

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ listItems.forEach(function(item) {
       burgerList.classList.add('hidden');
     });
   });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  const signUpBtn = document.getElementById('signUpLink');
+  const loginBtn = document.getElementById('loginLink')
+  const login = document.getElementById('loginWrap');
+  const signUp = document.getElementById('signUpWrap');
+
+// Hides login or signup section
+  signUpBtn.onclick = function(){
+    login.style.zIndex = '0';
+    login.style.opacity = '0';
+    signUp.style.opacity = '1';
+    signUp.style.zIndex = '1';
+  }
+
+  loginBtn.onclick = function(){
+    login.style.zIndex = "1";
+    login.style.opacity = "1";
+    signUp.style.opacity = "0";
+    signUp.style.zIndex = "0";
+  }
+  
+
+});
+
+
+
+
+
+
